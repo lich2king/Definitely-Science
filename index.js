df172f7e12a7bf949575e392ce50348a076b51f3
lich2king <lich2king@yahoo.com>
2024-07-08 17:08:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,29 @@ import wisp from "wisp-server-node";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
+
 app.use(express.static("public"));
 app.use("/uv/", express.static(uvPath));
 app.use("/epoxy/", express.static(epoxyPath));
 app.use("/baremux/", express.static(baremuxPath));
+
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Max-Age", "86400");
+    res.status(204).end();
+  } else {
+    next();
+  }
+});
+
 
 // Error for everything else
 app.use((req, res) => {
