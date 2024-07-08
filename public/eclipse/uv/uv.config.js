cef2e72a31d832b5746ebccfba40f9e3227845f3
lich2king <lich2king@yahoo.com>
2024-07-09 02:28:17 +0300

    gg

---
+++ public/eclipse/uv/uv.config.js
@@ -0,0 +1,12 @@
+/*global Ultraviolet*/
+self.__uv$config = {
+    prefix: '/eclipse/service/',
+    bare: '/ov/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/eclipse/uv/uv.handler.js',
+    client: '/eclipse/uv/uv.client.js',
+    bundle: '/eclipse/uv/uv.bundle.js',
+    config: '/eclipse/uv/uv.config.js',
+    sw: '/eclipse/uv/uv.sw.js',
+};
