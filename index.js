d79d3ff9195d4f6341eb625c51743b05eba7ecc7
lich2king <lich2king@yahoo.com>
2024-07-09 21:07:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import cors from "cors";
 
 import path from 'path';
 import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
