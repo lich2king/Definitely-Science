70a0dad6ec3b93dc88147d27431fbac9a8eba6b7
lich2king <lich2king@yahoo.com>
2024-07-09 03:24:30 +0300

    Revert "Update index.js"
    This reverts commit 3aa301fa124d45af5bb2cc0acb8cdd76dde33717.

--- index.js
+++ index.js
@@ -7,20 +7,13 @@ import { join } from "node:path";
 import { hostname } from "node:os";
 import wisp from "wisp-server-node";
 
-import { createBareServer } from "@tomphttp/bare-server-node";
-import cors from "cors";
-
-
 const app = express();
-const bareServer = createBareServer("/ov/")
 
 
 app.use(express.static("public"));
 app.use("/uv/", express.static(uvPath));
 app.use("/epoxy/", express.static(epoxyPath));
 app.use("/baremux/", express.static(baremuxPath));
-
-app.use("ov", cors({ origin: true }));
 
 
 // Error for everything else
@@ -32,24 +25,14 @@ app.use((req, res) => {
 const server = createServer();
 
 server.on("request", (req, res) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res);
-  } else {
-    app(req, res);
-  }
-})
+  app(req, res);
+});
 
 server.on("upgrade", (req, socket, head) => {
   if (req.url.endsWith("/wisp/"))
     wisp.routeRequest(req, socket, head);
   else
-  {
-	  if (bareServer.shouldRoute(req)) {
-		bareServer.routeUpgrade(req, socket, head);
-	  } else {
-		socket.end();
-	  }
-  }
+    socket.end();
 });
 
 let port = parseInt(process.env.PORT || "");
