1fb3111fcc3147d862ec9812ced7ef850eefef3d
lich2king <lich2king@yahoo.com>
2024-07-09 15:12:15 +0300

    Revert "epoxy"
    This reverts commit e2365061deb7b5da34ad59a9aeb57319854ab25b.

---
+++ public/eclipse/-/config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/eclipse/',
+    bare: '/ov/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/eclipse/-/handler.js?v=5-5-2024',
+    bundle: '/eclipse/-/bundle.js?v=5-5-2024',
+    config: '/eclipse/-/config.js?v=5-5-2024',
+    sw: '/eclipse/-/sw.js?v=5-5-2024',
+};
