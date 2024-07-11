f4462025d2ab7e3d80bb98f3302331fc524d28ff
lich2king <lich2king@yahoo.com>
2024-07-11 20:41:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-import { axios } from 'axios';
-import { cheerio } from 'cheerio';
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
