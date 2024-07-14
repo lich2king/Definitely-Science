21c8b51100754ea7b2a465e2bc706d839cd854b9
lich2king <lich2king@yahoo.com>
2024-07-14 14:43:48 +0300

    g

--- public/enigma/register-sw.js
+++ public/enigma/register-sw.js
@@ -1,18 +1,11 @@
 "use strict";
-/**
- * Distributed with Ultraviolet and compatible with most configurations.
- */
+
 const stockSW = "/uv/sw.js";
 
-/**
- * List of hostnames that are allowed to run serviceworkers on http://
- */
+
 const swAllowedHostnames = ["localhost", "127.0.0.1"];
 
-/**
- * Global util
- * Used in 404.html and index.html
- */
+
 async function registerSW() {
   if (!navigator.serviceWorker) {
     if (
@@ -26,7 +19,6 @@ async function registerSW() {
 
   await navigator.serviceWorker.register(stockSW);
 
-  // Register the EpoxyClient transport to be used for network requests
   let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
   await BareMux.SetTransport("EpxMod.EpoxyClient", { wisp: wispUrl });
 }
