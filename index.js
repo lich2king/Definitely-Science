6c7ae35d094a9f0c0d3ecd858754866421c7fbfb
lich2king <lich2king@yahoo.com>
2024-07-15 02:00:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,16 @@ function readFileContent(filePath) {
   });
 }
 
+app.use((req, res, next) => {
+    res.set({
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+        'Surrogate-Control': 'no-store'
+    });
+    next();
+});
+
 app.get('/class/:className', async (req, res) => {
   const className = req.params.className;
   try {
