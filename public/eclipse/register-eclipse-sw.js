ebf063f5f895bb8401341584d2fc32ebbb47b055
lich2king <lich2king@yahoo.com>
2024-07-17 19:53:44 +0300

    g

--- public/eclipse/register-eclipse-sw.js
+++ public/eclipse/register-eclipse-sw.js
@@ -2,26 +2,25 @@
 /**
  * eclipse sw
  */
-const eclipseSW = "/eclipse/sw.js";
 
-const swAllowedHostnames = ["localhost", "127.0.0.1"];
+const allowedHostnames = ["localhost", "127.0.0.1"];
 
 async function registerEclipseSW() {
   if (!navigator.serviceWorker) {
     if (
       location.protocol !== "https:" &&
-      !swAllowedHostnames.includes(location.hostname)
+      !allowedHostnames.includes(location.hostname)
     )
       throw new Error("Service workers cannot be registered without https.");
 
     throw new Error("Your browser doesn't support service workers.");
   }
 
-	navigator.serviceWorker.register(eclipseSW, {
+	navigator.serviceWorker.register("/eclipse/sw.js", {
 		scope: "/eclipse/",
 	  })
 
-  //await navigator.serviceWorker.register(stockSW);
+  //await navigator.serviceWorker.register(SW);
 
   // Register the EpoxyClient transport to be used for network requests
   //let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
--- public/enigma/register-sw.js
+++ public/enigma/register-sw.js
@@ -1,23 +1,23 @@
 "use strict";
+/**
+ * enigma sw
+ */
 
-const stockSW = "/uv/sw.js";
-
-
-const swAllowedHostnames = ["localhost", "127.0.0.1"];
+const allowedHostnames = ["localhost", "127.0.0.1"];
 
 
 async function registerSW() {
   if (!navigator.serviceWorker) {
     if (
       location.protocol !== "https:" &&
-      !swAllowedHostnames.includes(location.hostname)
+      !allowedHostnames.includes(location.hostname)
     )
       throw new Error("Service workers cannot be registered without https.");
 
     throw new Error("Your browser doesn't support service workers.");
   }
 
-  await navigator.serviceWorker.register(stockSW);
+  await navigator.serviceWorker.register("/uv/sw.js");
 
   let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
   await BareMux.SetTransport("EpxMod.EpoxyClient", { wisp: wispUrl });
