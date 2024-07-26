756eb972e2b4b9d6489cf8f14f4cad034367f794
lich2king <lich2king@yahoo.com>
2024-07-26 04:10:08 +0300

    Revert "g"
    This reverts commit cdeeb69c8d62de601f900f8e7b13a03681623201.

--- public/enigma/sw.js
+++ public/enigma/sw.js
@@ -1,9 +1,8 @@
 importScripts("/epoxy/index.js");
 importScripts("/enigma/bareTransport.js");
-importScripts("/enigma/uv.bundle.js");
-importScripts("/enigma/uv.config.js");
-//importScripts(__uv$config.sw || "uv.sw.js");
-importScripts("/enigma/uv.sw.js");
+importScripts("uv.bundle.js");
+importScripts("uv.config.js");
+importScripts(__uv$config.sw || "uv.sw.js");
 
 const uv = new UVServiceWorker();
 
